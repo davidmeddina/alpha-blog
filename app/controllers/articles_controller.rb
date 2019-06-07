b9f58feaf6c4f2984b4c34853b9345d21b279974
david medina <davidmeddina@gmail.com>
2019-06-07 18:22:36 -0500

    Add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,34 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: %i[edit update show destroy]
 
   def index
-  @articles = Article.all
+    @articles = Article.all
   end
 
   def new
-  @article = Article.new
+    @article = Article.new
   end
 
   def create
-  @article = Article.new(article_params)
-  @article.user = User.first
+    @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
-      flash[:success] = "Article was succesfully created"
+      flash[:success] = 'Article was succesfully created'
       redirect_to articles_path(@article)
     else
       render :new
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was succesfully updated"
+      flash[:success] = 'Article was succesfully updated'
       redirect_to articles_path(@article)
     else
       render :new
@@ -36,18 +36,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-  @article.destroy
-  flash[:danger] = "Articles was deleted"
-  redirect_to articles_path
+    @article.destroy
+    flash[:danger] = 'Articles was deleted'
+    redirect_to articles_path
   end
 
   private
 
   def set_article
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def article_params
-  params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,21 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_221929) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_id"
+ActiveRecord::Schema.define(version: 20_190_607_221_929) do
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.integer 'user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
